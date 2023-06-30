6c3a359b097411421f75a2cd97aae7f3f0c33919
lofi-J <jsj2505@naver.com>
2023-06-30 17:32:05 +0900

    change mysql server

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 module.exports = {
     user: {
-        host: process.env.DB_host,
-        user: process.env.DB_user,
-        password: process.env.DB_password,
-        database: process.env.DB_database
+        host: process.env.MYSQLHOST,
+        user: process.env.MYSQLUSER,
+        password: process.env.MYSQLPASSWORD,
+        database: process.env.MYSQLDATABASE
     }
 }

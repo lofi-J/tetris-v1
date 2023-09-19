0383404ce75ce9e8572e1657b8860e5f5e534d52
lofi-J <jsj2505@naver.com>
2023-09-20 06:32:15 +0900

    fix tetris text in home

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # React, Express(node.js), mysql
 
-## 테트리스 싱글 플레이와 점수(랭킹) 시스템 개발
+## 테트리스 싱글 플레이와 점수(랭킹) 시스템
 
 ### [WEB TETRIS](https://tetris-v1-production.up.railway.app/)
 
--- tetris/src/components/Help.js
+++ tetris/src/components/Help.js
@@ -1,38 +1,53 @@
 import { useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleQuestion, faSquareCaretLeft, faSquareCaretRight, faSquareCaretUp, faSquareCaretDown } from "@fortawesome/free-regular-svg-icons";
+import {
+  faCircleQuestion,
+  faSquareCaretLeft,
+  faSquareCaretRight,
+  faSquareCaretUp,
+  faSquareCaretDown,
+} from "@fortawesome/free-regular-svg-icons";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import "../css/Help.css";
 
 const Help = () => {
-    const [isClick, setIsClick] = useState(false);
-    const onClickToggle = () => {
-        if(isClick) setIsClick(false);
-        else setIsClick(true);
-    }
-    return (
-        <div className="help">
-            {!isClick ? <FontAwesomeIcon icon={faCircleQuestion} className="question" onClick={onClickToggle} /> : 
-            (<>
-            <div className="icons">
-                <FontAwesomeIcon icon={faSquareCaretLeft} />
-                <FontAwesomeIcon icon={faSquareCaretRight} />
-                <FontAwesomeIcon icon={faSquareCaretUp} />
-                <FontAwesomeIcon icon={faSquareCaretDown} />
-                <div className="spacebar">SPACE</div>
-            </div>
+  const [isClick, setIsClick] = useState(false);
+  const onClickToggle = () => {
+    setIsClick((prev) => !prev);
+  };
+  return (
+    <div className="help">
+      {!isClick ? (
+        <FontAwesomeIcon
+          icon={faCircleQuestion}
+          className="question"
+          onClick={onClickToggle}
+        />
+      ) : (
+        <FontAwesomeIcon icon={faCaretDown} className="question" onClick={onClickToggle} />
+      )}
+      {isClick && (
+        <div className="help__box">
+          <div className="icons">
+            <FontAwesomeIcon icon={faSquareCaretLeft} />
+            <FontAwesomeIcon icon={faSquareCaretRight} />
+            <FontAwesomeIcon icon={faSquareCaretUp} />
+            <FontAwesomeIcon icon={faSquareCaretDown} />
+            <div className="spacebar">SPACE</div>
+          </div>
 
-            <div className="texts">
-                <span>Move Left</span>
-                <span>Move Right</span>
-                <span>Rotate Right</span>
-                <span>Soft Drop</span>
-                <span>Hard Drop</span>
-            </div></>)
-            }
-
+          <div className="texts">
+            <span>Move Left</span>
+            <span>Move Right</span>
+            <span>Rotate Right</span>
+            <span>Soft Drop</span>
+            <span>Hard Drop</span>
+          </div>
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
 export default Help;
